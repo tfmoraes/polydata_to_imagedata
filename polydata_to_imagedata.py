a77a9ed9fcbc47a5a8cac75dfe4c0d3a02aacef6
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-11-05 11:18:57 -0300

    Added license

--- polydata_to_imagedata.py
+++ polydata_to_imagedata.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python3
+
+#  Copyright (c) Thiago Franco de Moraes
+#
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
 import math
 import sys
 
